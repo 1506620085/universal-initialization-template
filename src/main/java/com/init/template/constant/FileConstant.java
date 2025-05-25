57a978bd9805ae13842501cc3c88d1187563ee2c
Hangz <1506620085@qq.com>
2025-05-25 14:50:40 +0800

    初始化项目

---
+++ src/main/java/com/init/template/constant/FileConstant.java
@@ -0,0 +1,13 @@
+package com.init.template.constant;
+
+/**
+ * 文件常量
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://hangz.icu";
+}
---
+++ src/main/java/com/init/template/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.init.template.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.init.template.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
